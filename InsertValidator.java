0e6a73fe2f14a24b7a107b7301cd570bb39ff643
Pallavi-18-07-1999 <66680113+Pallavi-18-07-1999@users.noreply.github.com>
2020-07-11 16:31:51 +0530

    Delete InsertValidator.java

--- InsertValidator.java
+++
@@ -1,7 +0,0 @@
-package com.thinking.machines.interfaces;
-import com.thinking.machines.SupportClasses.*;
-public interface InsertValidator
-{
-public boolean success(TableSetter ts,Object obj);
-public String validationFailedMessage();
-}
